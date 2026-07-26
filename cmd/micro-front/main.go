8edf1a3ecf97ed9ac0513f0ea19f201c00210eca
multios12 <5012052+multios12@users.noreply.github.com>
2026-07-27 07:44:20 +0900

    add: time zone setting.

--- cmd/micro-front/main.go
+++ cmd/micro-front/main.go
@@ -4,7 +4,27 @@ import (
 	"context"
 	"log"
 	"os"
+	"time"
+	_ "time/tzdata"
 )
+
+func init() {
+	// 1. 環境変数「TZ」から値を取得（設定されていなければデフォルト値を設定）
+	tzEnv := os.Getenv("TZ")
+	if tzEnv == "" {
+		tzEnv = "Asia/Tokyo" // デフォルト値
+	}
+
+	// 2. タイムゾーン名からLocationオブジェクトを生成
+	loc, err := time.LoadLocation(tzEnv)
+	if err != nil {
+		// 変換に失敗した場合は、安全のためUTCや固定値を割り当てる
+		loc = time.UTC
+	}
+
+	// 3. グローバルのローカルタイムゾーンを上書き
+	time.Local = loc
+}
 
 func main() {
 	log.SetFlags(0)
